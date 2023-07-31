27ef59f3cfed34c6a76f65c9e62f837dff73da17
DESKTOP\\VVRON <me@aaronakrong.com>
2023-07-31 18:15:47 +0100

    initial clock script commit

---
+++ Assets/Clock.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+/// <summary>
+/// Component to display current time on clock object
+/// </summary>
+public class Clock : MonoBehaviour
+{
+    private void Update()
+    {
+
+    }
+}
